ef2194780145ce3b4f5e5a6554a426c768524d25
Yasinyudha <yasinyudha20@gmail.com>
2025-05-09 15:41:36 +0700

    improve process_data.sh

--- process_data.sh
+++ process_data.sh
@@ -1,38 +1,7 @@
-#!/bin/bash
+python delete_all.py
+python edit_tif_manifest.py
+python edit_tif.py
+python clip_tiff.py
 
-# Make sure all Python scripts are executable
-chmod +x *.py
-
-# Remove all non-.zip files in the hyp3 directory
-NON_ZIP=./hyp3/*Zone.Identifier
-rm -rf "${NON_ZIP[@]}"
-
-# Run extract.py and capture its exit status
-python extract.py
-STATUS=$?
-
-if [ $STATUS -ne 0 ]; then
-  echo "Error: extract.py failed! Exiting."
-  exit 1
-fi
-
-echo "Extract.py completed successfully."
-
-# Check if the dataset contains clipped TIFF files
-DIR_FILES=(./hyp3/*)
-
-if [ -d "${DIR_FILES[0]}" ]; then
-  for directory in "${DIR_FILES[@]}"; do
-    ZIP_FILES=("$directory"/*clipped.tif)
-
-    if [ ${#ZIP_FILES[@]} -eq 0 ]; then
-      # Run clip_tiff.py
-      python clip_tiff.py
-    else
-      # Delete first, then clip again
-      python delete.py
-      python clip_tiff.py
-    fi
-    break
-  done
-fi
+cd mintpy/
+smallbaselineApp.py ../madura_full.txt
